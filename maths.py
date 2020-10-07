7baa0beeee72163a8864a020ee1724e82b68e7ec
Idajojeff <72307935+Idajojeff@users.noreply.github.com>
2020-10-07 17:47:10 +0100

    added fibonacci

---
+++ maths.py
@@ -0,0 +1,9 @@
+#web.facebook.com/idajo.jeff/posts/128970875635248
+#subscribed by code house
+def fib(n):
+    a, b = 0, 1
+while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+fib(1000)
